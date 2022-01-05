9ba8998efba4efc15491f0b37c99b152dc48b481
jimmyhealer <yahing6066@gmail.com>
2022-01-06 03:45:58 +0800

    update gitpage

--- firebase.js
+++ firebase.js
@@ -1,11 +1,11 @@
 import { initializeApp } from "firebase/app";
 import { addDoc, getFirestore } from "firebase/firestore";
 import { doc, onSnapshot } from "firebase/firestore";
-import { collection, setDoc } from "firebase/firestore";
+import { getDoc, setDoc } from "firebase/firestore";
 import {
   getAuth,
   signInWithEmailAndPassword,
-  onAuthStateChanged
+  onAuthStateChanged,
 } from "firebase/auth";
 
 const firebaseConfig = {
@@ -21,29 +21,71 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
+export var nowGroupId;
 
-const groupID = onSnapshot(doc(db, "now", "now"), (doc) => {
-  $("#group").html(doc.data().group);
-  if(doc.data().group === doc.data().end){
-    $("#beginRating").hide();
-    $("#ratingContainer").show();
-  }
-  else{
-    $("#beginRating").show();
-    $("#ratingContainer").hide();
-  }
-});
-
-async function uploadScore(id, data) {
+async function uploadScore(id, data, identity) {
   let s = doc(db, "group", `group_${id}`);
-  let b = collection(s, "student_scores_set");
-  await addDoc(b, data);
+  let b = doc(s, `identity${identity}`, $.cookie("UUID"));
+  console.log(data);
+  await setDoc(b, data);
 }
 
 export const auth = getAuth();
 
 function login(password) {
   return signInWithEmailAndPassword(auth, `${password}@fuyajo.tw`, password);
+}
+
+const groupID = onSnapshot(doc(db, "now", "now"), (doc) => {
+  init(doc);
+  $(".loading-overlay").hide();
+});
+
+
+function init(doc){
+  let id = doc.data().end;
+  nowGroupId = doc.data().group;
+  if(id > 0) $("#group").html(`目前組別 第${nowGroupId}組`);
+  else if(id < 0) $("#group").html(`休息中`);
+  else if(id == 0) $("#group").html(`晚上 6 點 40 準時開始`);
+  if (doc.data().group === doc.data().end) {
+    $("#beginRating").hide();
+    $("#ratingContainer").show();
+    let tmp = $.cookie(`group${id}`);
+    if (tmp !== undefined) {
+      $("input[name=rating0]")
+        .filter(`[value=${parseInt(tmp / 1000)}]`)
+        .prop("checked", true);
+      $("input[name=rating1]")
+        .filter(`[value=${parseInt(tmp / 100) % 10}]`)
+        .prop("checked", true);
+      $("input[name=rating2]")
+        .filter(`[value=${parseInt(tmp / 10) % 10}]`)
+        .prop("checked", true);
+      $("input[name=rating3]")
+        .filter(`[value=${tmp % 10}]`)
+        .prop("checked", true);
+      $("#typeNumber0").val(parseInt(tmp / 1000000));
+      $("#typeNumber1").val(parseInt(tmp / 10000) % 100);
+      $("#typeNumber2").val(parseInt(tmp / 100) % 100);
+      $("#typeNumber3").val(parseInt(tmp % 100));
+      $("#comment").val($.cookie(`group${id}_C`));
+      if(!auth.currentUser) {
+        $("input").prop("disabled", true);
+        $("textarea").prop("disabled", true);
+        $("#ratingSubmit").prop("disabled", true);
+      }
+      $("#msg").html("評分成功");
+    }
+  } else {
+    $("#beginRating").show();
+    $("#ratingContainer").hide();
+    $("input").prop("checked", false);
+    $("input").prop("disabled", false);
+    $("textarea").prop("disabled", false);
+    $("#comment").val("");
+    $("#ratingSubmit").prop("disabled", false);
+  }
 }
 
 export { uploadScore, login };
--- index.js
+++ index.js
@@ -1,7 +1,25 @@
-import { uploadScore, login, currentUser, auth } from "./firebase.js";
+import { uploadScore, login, nowGroupId, auth } from "./firebase.js";
 import { onAuthStateChanged } from "firebase/auth";
+import { Modal } from "./assets/js/bootstrap.esm.min.js";
 
-var loginState = 0;
+export var loginState = 0;
+var isTeacher = 0;
+const UUID = _uuid();
+
+function _uuid() {
+  var d = Date.now();
+  if (
+    typeof performance !== "undefined" &&
+    typeof performance.now === "function"
+  ) {
+    d += performance.now();
+  }
+  return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, function (c) {
+    var r = (d + Math.random() * 16) % 16 | 0;
+    d = Math.floor(d / 16);
+    return (c === "x" ? r : (r & 0x3) | 0x8).toString(16);
+  });
+}
 
 const a = $("#ratingSubmit").click((e) => {
   e.preventDefault();
@@ -10,16 +28,54 @@ const a = $("#ratingSubmit").click((e) => {
     .parent()
     .parent()
     .removeClass("error");
-  var score =
-    $("input[name=rating0]:checked").val() * 5 +
-    $("input[name=rating1]:checked").val() * 11 +
-    $("input[name=rating2]:checked").val() * 3 +
-    $("input[name=rating3]:checked").val() * 1;
+  let score;
+  if (!loginState)
+    score =
+      $("input[name=rating0]:checked").val() * 5 +
+      $("input[name=rating1]:checked").val() * 11 +
+      $("input[name=rating2]:checked").val() * 3 +
+      $("input[name=rating3]:checked").val() * 1;
+  else
+    score = 
+      $("input#typeNumber0").val() * 1+
+      $("input#typeNumber1").val() * 1+
+      $("input#typeNumber2").val() * 1+
+      $("input#typeNumber3").val() * 1;
+  let tmpCookie;
+  if (!loginState)
+    tmpCookie =
+      $("input[name=rating0]:checked").val() * 1000 +
+      $("input[name=rating1]:checked").val() * 100 +
+      $("input[name=rating2]:checked").val() * 10 +
+      $("input[name=rating3]:checked").val() * 1;
+  else tmpCookie = 
+      $("#typeNumber0").val() * 1000000 +
+      $("#typeNumber1").val() * 10000 +
+      $("#typeNumber2").val() * 100 +
+      $("#typeNumber3").val() * 1;
+  console.log(tmpCookie);
+  let group_id = nowGroupId;
   if (!Number.isNaN(score)) {
-    uploadScore($("#group").val(), {score: score, comment: $("#comment").val()});
-    $("input").prop("disabled", true);
-    $("textarea").prop("disabled", true);
-    $("#ratingSubmit").prop("disabled", true);
+    console.log(group_id);
+    let identity;
+    if (loginState) {
+      identity = isTeacher ? 1 : 0;
+    } else {
+      identity = 2;
+    }
+    uploadScore(group_id, {
+      score: score,
+      comment: $("#comment").val(),
+    }, identity);
+    console.log(tmpCookie);
+    $.cookie(`group${group_id}`, tmpCookie, { expires: 1 });
+    $.cookie(`group${group_id}_C`, $("#comment").val(), { expires: 1 });
+    $("#msg").html("評分成功");
+    if(!loginState) {
+      $("input").prop("disabled", true);
+      $("textarea").prop("disabled", true);
+      $("#ratingSubmit").prop("disabled", true);
+    }
   } else {
     $("input:radio:not(:checked)", this)
       .parent()
@@ -30,36 +86,57 @@ const a = $("#ratingSubmit").click((e) => {
   }
 });
 
+function hideModal() {
+  $("#loginModal").removeClass("in");
+  $(".modal-backdrop").remove();
+  $("body").removeClass("modal-open");
+  $("body").css("padding-right", "");
+  $("#loginModal").hide();
+}
+
 $("#login").submit((e) => {
   e.preventDefault();
   $("#loginBtn").prop("disabled", true);
   $("#passwordInput").prop("disabled", true);
   login($("#passwordInput").val())
     .then((res) => {
-      $("#loginModal").model("hide");
+      hideModal();
     })
     .catch((err) => {
-      
+      $("#passwordInput").addClass("is-invalid");
+      $("#passwordInput").focus();
+      console.log(err);
     })
     .finally(() => {
       $("#loginBtn").prop("disabled", false);
       $("#passwordInput").prop("disabled", false);
     });
-})
+});
 
 onAuthStateChanged(auth, (user) => {
-  if(user){
+  if (user) {
+    isTeacher = user.email === "fuyajoteacher0106!@fuyajo.tw";
+    loginState = 1;
     $("#loginNavBtn").hide();
-    $("#userName").html(user.email);
+    if (!isTeacher) $("#userName").html(user.email);
+    else $("#userName").html("馬尚彬 教授");
+    $(".star").hide();
+    $(".number").show();
     $("#dashborad").show();
     $("#ratingContainer").show();
     $("#beginRating").hide();
-    loginState = 1;
-  }
-  else{
+  } else {
+    $(".number").hide();
+    $(".star").show();
     $("#loginNavBtn").show();
-    $("#userName").html('');
+    $("#userName").html("");
     $("#dashborad").hide();
     loginState = 0;
   }
 });
+
+if ($.cookie("UUID") === undefined) {
+  $.cookie("UUID", UUID, { expires: 365 });
+}
+
+$("#uuid").html(`You UUID is ${$.cookie("UUID")}`);
